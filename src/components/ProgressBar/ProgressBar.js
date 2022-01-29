8ce3187fc81132f5972281831dfba61c98ba4292
Chris Shorter <shorte24@gmail.com>
2022-01-29 15:46:48 -0500

    Implements ARIA values for scrollbar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,8 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+function ProgressBar({ value, size }) {
+  return <div role="progressbar" aria-valuenow={size.toString()} aria-valuemin="0" aria-valuemax="100">{value}</div>
 };
 
 export default ProgressBar;
